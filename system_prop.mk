090256df086703a00d57d24b8122c934c4d90b26
David <davidmolinamorales1999@gmail.com>
2022-09-30 23:05:15 +0200

    j5lte: Replace BTM_DEF_LOCAL_NAME with a prop

--- system_prop.mk
+++ system_prop.mk
@@ -11,3 +11,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # RAM
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.low_ram=false
+	
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+	bluetooth.device.default_name=Galaxy J5 (SM-J500F/G/M/NO/Y)
